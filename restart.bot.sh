f7c38d6ce3d0b32541c854bd9d9bb0d934d76c71
roughnecks <roughnecks@laltromondo.dynalias.net>
2013-06-29 22:49:30 +0200

    patched restart.bot.sh: quotes and gzipped logfiles. Thanks skizzhg

--- restart.bot.sh
+++ restart.bot.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-if [ ! -f "$1" ]; then
-    echo "Usage: $0 configurationfile.conf"
+if [[ ! -f "$1" ]]; then
+    printf '%s\n' "Usage: $0 configurationfile.conf"
 fi
 
-botdir=$(dirname $0)
+botdir="$(dirname $0)"
 
-if [ ! -d "$botdir/logs" ]; then
-	mkdir -p "$botdir/logs"
-fi
+mkdir -p "$botdir/logs"
 
-logfile=$botdir/logs/ph.log
+logfile="$botdir/logs/ph.log"
 
-cd $botdir || exit 2
+cd "$botdir" || exit 2
 
 # the bot never started, as there is no ph.pid, so append to logfile
-if [ ! -f "$botdir/ph.pid" ]; then
-    exec nohup perl planthopper.pl $1 >> "$logfile" 2>&1 &
+if [[ ! -f "$botdir/ph.pid" ]]; then
+    exec nohup perl planthopper.pl "$1" >> "$logfile" 2>&1 &
     exit
 fi
 
-pid=$(cat $botdir/ph.pid)
+# check if the process exists
+pid="$(< $botdir/ph.pid)"
 
-if kill -0 $pid > /dev/null 2>&1 ; then
+if kill -0 "$pid" > /dev/null 2>&1; then
     exit
 fi
 
 # rotate the logs
-if [ -f "$logfile" ]; then
-    cat $logfile >> $logfile-`date +%F`;
-    : > $logfile;
+if [[ -f "$logfile" ]]; then
+    cat "$logfile" | gzip > "$logfile-$(date +%T_%F).gz" && > "$logfile"
 fi
 
-echo -n $(date) >> "$logfile"
-echo "Bot restarted by $0" >> "$logfile"
-exec nohup perl planthopper.pl $1 >> "$logfile" 2>&1 &
-
-
-
-
+printf "$(date)\n" >> "$logfile"
+printf '%s\n' "Bot restarted by $0" >> "$logfile"
+exec nohup perl planthopper.pl "$1" >> "$logfile" 2>&1 &
